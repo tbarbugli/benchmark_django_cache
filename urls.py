f13a4bef6a2f899106cfda0858378cb27fe6a8ba
Tommaso Barbugli <tbarbugli@gmail.com>
2012-08-01 14:45:54 +0200

    remove base module from url imports

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^test_one_get/$', 'cachetest.frontend.views.test_one_get'),
-    url(r'^test_multiple_gets/$', 'cachetest.frontend.views.test_multiple_gets'),
-    url(r'^test_multiple_duplicated_gets/$', 'cachetest.frontend.views.test_multiple_duplicated_gets'),
+    url(r'^test_one_get/$', 'frontend.views.test_one_get'),
+    url(r'^test_multiple_gets/$', 'frontend.views.test_multiple_gets'),
+    url(r'^test_multiple_duplicated_gets/$', 'frontend.views.test_multiple_duplicated_gets'),
 )
