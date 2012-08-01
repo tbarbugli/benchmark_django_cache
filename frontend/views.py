b3668cde64578b249e5f89935f1d9606c080c9e0
Tommaso Barbugli <tbarbugli@gmail.com>
2012-08-01 14:42:03 +0200

    better random cache keys

--- frontend/views.py
+++ frontend/views.py
@@ -1,27 +1,32 @@
+import string
+from random import choice
 from django.core.cache import cache
 from django.http import HttpResponse
-import os
+
+def random_str():
+    chars = string.letters + string.digits
+    return ''.join([choice(chars) for i in xrange(15)])
 
 def test_one_get(request):
-    cache.get(os.urandom(15))
+    cache.get(random_str())
     return HttpResponse("Hello World!")
 
 def test_multiple_gets(request):
-    cache.get(os.urandom(15))
-    cache.get(os.urandom(15))
-    cache.get(os.urandom(15))
-    cache.get(os.urandom(15))
-    cache.get(os.urandom(15))
-    cache.get(os.urandom(15))
+    cache.get(random_str())
+    cache.get(random_str())
+    cache.get(random_str())
+    cache.get(random_str())
+    cache.get(random_str())
+    cache.get(random_str())
     return HttpResponse("Hello World!")
 
 def test_multiple_duplicated_gets(request):
     """
     test in memory cache
     """
-    a = os.urandom(15)
-    b = os.urandom(15)
-    c = os.urandom(15)
+    a = random_str()
+    b = random_str()
+    c = random_str()
     cache.get(a)
     cache.get(b)
     cache.get(c)
