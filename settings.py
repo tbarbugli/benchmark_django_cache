fef7f1389116e5bafb3c01e006f48c888b6701b2
Tommasa Barbugli <tommaso@fashiolista.com>
2012-08-01 12:50:21 +0000

    wrong name for base module urls

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'cachetest.urls'
+ROOT_URLCONF = 'benchmark_django_cache.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
